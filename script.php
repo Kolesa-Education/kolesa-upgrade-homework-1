0f2012f801e061810e47babd505a86fc36c758a7
Askar <askxrqq@gmail.com>
2022-09-15 01:12:51 +0600

    выполнил задание 1

---
+++ script.php
@@ -0,0 +1,31 @@
+<?php
+$a = $_GET['a'];
+$b = $_GET['b'];
+$c = $_GET['c'];
+$sql = "INSERT INTO test(a, b, c)
+VALUES ('$a','$b','$c')";
+switch ($c) {
+    case "+":
+        echo $a + $b;
+        break;
+    case "-":
+        echo $a + $b;
+        break;
+    case "*":
+        echo $a + $b;
+        break;
+    
+    case "/":
+        echo $a + $b;
+        break;
+	case "**":
+        echo $a ** $b;
+        break;
+	case "//":
+        echo $a % $b;
+        break;
+	default:
+        echo "не правильный запрос";
+        break;
+}
+?>
