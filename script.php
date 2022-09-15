a70de22fbc6fed8467fe2fc0c56429dee4289d93
Askar <askxrqq@gmail.com>
2022-09-15 10:26:36 +0600

    улучшил код

--- script.php
+++ script.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic" rel="stylesheet" />
+</head>
+<body>
+    <h1>Калькулятор Аскара</h1>
+    <form action='script.php' method='get'>
+        <input type="number" name = "a" required>
+        <select name = "c" required>
+            <option value = "+"> сложение </option>
+            <option value = "-"> вычитание </option>
+            <option value = "/"> деление </option>
+            <option value = "*"> умножение </option>
+            <option value = "**"> степень </option>
+            <option value = "//"> модуль </option>
+        </select>
+        <input type="number" name = "b" required>
+        <input type='submit' value='Submit'>
+    </form>
+    <br/>
+</body>
+</html>
+
 <?php
 $a = $_GET['a'];
 $b = $_GET['b'];
@@ -43,31 +72,3 @@ switch ($c) {
         break;
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic" rel="stylesheet" />
-</head>
-<body>
-    <h1>Калькулятор Атая</h1>
-    <form action='script.php' method='get'>
-        <input type="number" name = "a" required>
-        <select name = "c" required>
-            <option value = "+"> сложение </option>
-            <option value = "-"> вычитание </option>
-            <option value = "/"> деление </option>
-            <option value = "*"> умножение </option>
-            <option value = "**"> степень </option>
-            <option value = "//"> модуль </option>
-        </select>
-        <input type="number" name = "b" required>
-        <input type='submit' value='Submit'>
-    </form>
-</body>
-</html>
-
