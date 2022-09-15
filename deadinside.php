1ea50048e02c462b860490499ddaf3d37a15605a
Askar <askxrqq@gmail.com>
2022-09-15 12:13:38 +0600

    улучшил функцию дединсайд

--- deadinside.php
+++ deadinside.php
@@ -1,9 +1,9 @@
 <?php
     require "index.html";
-    $ded = 1000;
+    $name = $_GET['name'];
     for ($x = 1000; $x > 0; $x-=7) {
         echo $x . ' - 7';
         echo '<br/>';
     }
-    echo 'я дед инсайд';
+    echo $name . ' дед инсайд';
 ?>
