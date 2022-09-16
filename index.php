c30fc3fbfbaf687d31d3da55cd969b327be98869
yltyadi <adi.yeltay@nu.edu.kz>
2022-09-16 19:02:04 +0600

    выполнил задание 1 2.0

--- index.php
+++ index.php
@@ -3,40 +3,42 @@ $a = $_GET['a'];
 $b = $_GET['b'];
 $c = $_GET['c']; // operation 
 
-if (is_numeric($a) && is_numeric($b) ) {
-    switch($c) {
-        case "+":
-            echo $a + $b;
-            break;
-        case "-":
-            echo $a - $b;
-            break;
-        case "/":
-            echo $a / $b;
-            break;
-        case "*":
-            echo $a * $b;
-            break;
-        case "^":
-            echo pow($a, $b);
-            break;
-        case "%":
-            echo $a % $b;
-            break;
-        case ">":
-            echo $a > $b;
-            break;
-        case "<":
-            echo $a < $b;
-            break;
-        case "=":
-            echo $a == $b;
-            break;
-        default:
-            echo "no such operation";
+if (!empty($a) && !empty($b) && !empty($b)) {
+    if (is_numeric($a) && is_numeric($b) ) {
+        switch($c) {
+            case "+":
+                echo $a + $b;
+                break;
+            case "-":
+                echo $a - $b;
+                break;
+            case "/":
+                echo ($b != 0) ? ($a / $b) : 'cannot divide by zero';
+                break;
+            case "*":
+                echo $a * $b;
+                break;
+            case "^":
+                echo pow($a, $b);
+                break;
+            case "%":
+                echo $a % $b;
+                break;
+            case ">":
+                echo ($a > $b) ? 'true' : 'false';
+                break;
+            case "<":
+                echo ($a < $b) ? 'true' : 'false';
+                break;
+            case "=":
+                echo ($a == $b) ? 'true' : 'false';
+                break;
+            default:
+                echo "no such operation";
+        }
+    } else {
+        echo "either a or b is not an integer or a float!";
     }
 } else {
-    echo "either a or b is not an integer or a float!";
+    echo "at least one of a, b, and c is undefined!";
 }
-
-?>
