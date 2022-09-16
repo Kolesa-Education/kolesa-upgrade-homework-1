890b137f6a97cf73eabe86c7241a003e1c630c3a
Asilkhan Tagaev <asik.tagaev15@gmail.com>
2022-09-16 16:16:22 +0600

    Исправил "+", валидацию параметров, убрал case "<"

--- index.php
+++ index.php
@@ -1,53 +1,56 @@
 <?php
 
+// %2B => +
+// %2A => *
+// %2F => /
+// -   => -
+// %25 => %
+// %3E => >
+
 $a = $_GET['a'];
 $b = $_GET['b'];
 $c = $_GET['c'];
 
-
-if (is_numeric($a) && is_numeric($b)) {
-    switch ($c) {
-        case " ":
-           echo $a + $b;
-            break;
-        case "-":
-           echo $a - $b;
-            break;
-        case "*":
-            echo $a * $b;
-            break;
-        case "^":
-            echo pow($a, $b);
-            break;
-        case "/":
-            echo $a / $b;
-            break;
-        case ">":
-            if ($a > $b) {
-                echo ($a . " больше чем " . $b);
-            } elseif ($a === $b) {
-                echo "Введенные значения равны";
-            } else {
-                echo $a . " меньше чем " . $b;
-            }
-            break;
-        case "<":
-            if ($a < $b) {
-                echo ($a . " меньше чем " . $b);
-            } elseif ($a === $b) {
-                echo "Введенные значения равны";
-            } else {
-                echo $a . " больше чем " . $b;
-            }
-            break;
-        case "%":
-            echo $b . "% из " . $a . " равно " . ($b / 100) * $a;
-            break;
-        default:
-            echo "Неправильная операция";
-    }
+if (!isset($a, $b, $c)) {
+  echo "Отсутствует параметр a, b или с";
+  exit;
+} elseif (!is_numeric($a) || !is_numeric($b)) {
+  echo "Параметры a и b должны быть цифрами";
+  exit;
 } else {
-    echo "Один или несколько введенных значении не являются цифрой";
-} 
+  echo "Валидация успешна \n";
+}
+
+switch ($c) {
+    case "+":
+       echo $a + $b;
+        break;
+    case "-":
+       echo $a - $b;
+        break;
+    case "*":
+        echo $a * $b;
+        break;
+    case "^":
+        echo pow($a, $b);
+        break;
+    case "/":
+        echo $a / $b;
+        break;
+    case ">":
+        if ($a > $b) {
+            echo ($a . " больше чем " . $b);
+        } elseif ($a === $b) {
+            echo "Введенные значения равны";
+        } else {
+            echo $a . " меньше чем " . $b;
+        }
+        break;
+    case "%":
+        echo $b . "% из " . $a . " равно " . ($b / 100) * $a;
+        break;
+    default:
+        echo "Неправильная операция";
+}
 
 ?>
