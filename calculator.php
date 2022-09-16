bd9c0d4d372a06731d5c998c9f95d16c007ee048
ArkadiyBelyakov <belyakovarkadiy@gmail.com>
2022-09-16 16:02:45 +0600

    добавил проверки

--- calculator.php
+++ calculator.php
@@ -5,14 +5,25 @@
 // %2F => /
 // -   => -
 
+if (empty($_GET)) {
+    echo "Ничего не передано";
+    exit(0);
+}
+
 $a = $_GET['a'];
 $b = $_GET['b'];
 $c = $_GET['c'];
 
+$operators = [ '+', '-', '/', '*', '**'];
+
 if ((!is_numeric($a))  or (!is_numeric($b))) {
     echo "Ошибка. Нужно ввести числа";
     exit();
-} else {
+}
+    else if (!in_array($c, $operators)){
+        echo "Неправильная операция {$c}, введите знаки математических операций";
+        exit();
+    } else {
     switch ($c) {
         case urldecode("+"):
             echo $a + $b;
@@ -30,8 +41,7 @@ if ((!is_numeric($a))  or (!is_numeric($b))) {
             echo $a ** $b;
             break;
         default:
-            echo "Неправильная операция {$c}, введите знаки математических операций";
+            exit;
     }
-
-}
+    }
 ?>
