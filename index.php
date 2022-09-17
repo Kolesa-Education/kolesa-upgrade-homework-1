0c0d19a278912d376dc130386a6479a168184886
DiasKaipan <dias.kaipan@icloud.com>
2022-09-17 13:19:46 +0600

    Вторые правки

--- index.php
+++ index.php
@@ -6,23 +6,25 @@
 // %2D => -
 // %25 => %
 
-
-if (isset($a) || isset($b) || isset($c) ){
-    $a = $_GET['a'];
-    $b = $_GET['b'];
-    $c = $_GET['c'];
-}
-if (empty($a) || empty($b) || empty($c) ) {
-    echo 'Отсутствует один из параметров a, b или c';
+if (!isset($_GET['a']) || !isset($_GET['b']) || !isset($_GET['c']) ){
+    echo 'Параметры не передаются';
     return;
-} elseif (!is_numeric($a) || !is_numeric($b)) {
+}
+
+$a = $_GET['a'];
+$b = $_GET['b'];
+$c = $_GET['c'];
+
+if (!is_numeric($a) || !is_numeric($b)) {
     echo 'Параметры a и b должны быть числами';
     return;
-} elseif ($c == '/' && $b == 0) {
+} 
+
+if ($c == '/' && $b == 0) {
     echo 'На ноль делить нельзя фу таким быть';
     return;
 }
-else {
+
 switch ($c):
     case "+":
         echo '$a + $b = ' . ($a + $b);
@@ -57,4 +59,3 @@ switch ($c):
     default:
         echo "Не правильная операция";
 endswitch;
-}
