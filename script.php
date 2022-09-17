879c9f13913428154994451e3c2abdd24a87a937
Askar <askxrqq@gmail.com>
2022-09-17 15:10:34 +0600

    пофиксил баги

--- script.php
+++ script.php
@@ -3,8 +3,6 @@ require "index.html";
 $a = $_GET['a'];
 $b = $_GET['b'];
 $c = $_GET['c'];
-$sql = "INSERT INTO index(a, b, c)
-VALUES ('$a','$b','$c')";
 echo 'ответ: ';
 switch ($c) {
     case "+":
@@ -23,14 +21,9 @@ switch ($c) {
         }  else {
             echo "на ноль делить нельзя";
         }
-        
         break;
 	case "**":
-        if ($a == 0 && $b < 0) {
-            echo "не определено";
-        }  else {
-            echo $a % $b;
-        }
+        echo $a ** $b;
         break;
 	case "//":
         if ($b != 0) {
