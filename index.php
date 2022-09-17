51d9d5f3ce903b3d0dbb980b977b6cb854bea2aa
Kanatov Nurzhas <nurzhaskanatovbarca@gmail.com>
2022-09-17 10:02:50 +0600

    Выполняю задание 1

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+$a=$_GET['a'];
+$b=$_GET['b'];
+$c=$_GET['c'];
+
+
+if (validateSet($a,$b,$c)){
+    if (is_numeric($a) && is_numeric($b)){
+        echo calculate($a,$b,$c);
+    }else{
+        echo 'Invalid data';
+    }
+    
+}else{
+    echo 'Data is empty';
+}
+
+
+function calculate($a,$b,$c){
+    switch ($c){
+    case "+":
+        return $a+$b;
+    case "-":
+        return $a-$b;
+    case "*":
+        return $a*$b;
+    case "/":
+        if ($b==0){
+            echo 'No division by 0';
+            return;
+        }else{
+            return $a/$b;
+        }
+    case "**":
+        return $a**$b;
+    case "%":
+        if ($b==0){
+            echo 'No modulo by 0';
+            return;
+        }else{
+            return $a%$b;
+        }
+    case ">":
+        if ($a>$b){
+            echo 'True';
+            return;
+        }else{
+            echo 'False';
+            return;
+        }
+    case "<":
+        if ($a<$b){
+            echo 'True';
+            return;
+        }else{
+            echo 'False';
+            return;
+        }
+    case "=":
+        if ($a==$b){
+            echo 'True';
+            return;
+        }else{
+            echo 'False';
+            return;
+        }
+    default:
+        return'Is not an operator';
+    }
+}
+
+
+function validateSet($a,$b,$c){
+    if (isset($a) && isset($b) && isset($c)){
+        return true;
+    }else{
+        return false;
+    }
+}
+?>
