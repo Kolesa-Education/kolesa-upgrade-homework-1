90282be247f505972bea7a1fe886c20236918390
nrakhay <nuralirakhay@gmail.com>
2022-09-18 14:21:28 +0600

    поставил условие для остановки скрипта

--- index.php
+++ index.php
@@ -4,63 +4,63 @@ $a = $_GET['a'];
 $b = $_GET['b'];
 $c = $_GET['c'];
 
-if (is_numeric($a) && is_numeric($b)) {
-        switch ($c) {
-            case "+":
-                echo $a + $b;
-                break;
-            case "-":
-                echo $a - $b;
-                break;
-            case "/":
-                if ($b == 0) {
-                    echo "Cannot divide by 0";
-                    break;
-                }
+if (!is_numeric($a) || !is_numeric($b)) {
+    exit("Non numeric arguments were provided");       
+}
 
-                echo $a / $b;
-                break;
-            case "*":
-                echo $a * $b;
-                break;
-            case "**":
-                $res = 1;
-                while ($b > 0) {
-                    $res = $res * $a;
-                    $b--;
-                }
-                
-                echo $res;
-                break;
-            case "==":
-                if ($a === $b){
-                    echo "Numbers are equal";
-                } else {
-                    echo "Numbers are not equal";
-                }
-
-                break;
-            case "%":
-                if ($b == 0){
-                    echo "Cannot divide by 0";
-                    break;
-                }
-                   
-                
-                if ($b > $a) {
-                    echo $a;
-                    break;                    
-                }
-
-                $div = floor($a / $b);
-                echo ($a - ($div * $b));
-                
-                break;
-
-            default:
-                echo "Wrong operation";
+switch ($c) {
+    case "+":
+        echo $a + $b;
+        break;
+    case "-":
+        echo $a - $b;
+        break;
+    case "/":
+        if ($b == 0) {
+            echo "Cannot divide by 0";
+            break;
         }
-    } else {
-        echo "Please, provide numbers";
-    }
+
+        echo $a / $b;
+        break;
+    case "*":
+        echo $a * $b;
+        break;
+    case "**":
+        $res = 1;
+        while ($b > 0) {
+            $res = $res * $a;
+            $b--;
+        }
+        
+        echo $res;
+        break;
+    case "==":
+        if ($a === $b){
+            echo "Numbers are equal";
+        } else {
+            echo "Numbers are not equal";
+        }
+
+        break;
+    case "%":
+        if ($b == 0){
+            echo "Cannot divide by 0";
+            break;
+        }
+            
+        
+        if ($b > $a) {
+            echo $a;
+            break;                    
+        }
+
+        $div = floor($a / $b);
+        echo ($a - ($div * $b));
+        
+        break;
+
+    default:
+        echo "Wrong operation";
+}
 ?>
